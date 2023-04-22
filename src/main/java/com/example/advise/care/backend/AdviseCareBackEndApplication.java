46268506bf9a5e2ae8abb63bb653959f4bed89a0
olili27 <timmolili27@gmail.com>
2023-04-23 01:56:42 +0300

    bootstrap the app with spring boot

---
+++ src/main/java/com/example/advise/care/backend/AdviseCareBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.advise.care.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdviseCareBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdviseCareBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/advise/care/backend/AdviseCareBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.advise.care.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdviseCareBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
